c387e842d7f949d22c490ad64d1aca4457e9f0d4
hope-18 <158341177+hope-18@users.noreply.github.com>
2025-12-09 19:57:06 -0500

    ajout du dossier exercice
    ajout du dossier exercice

---
+++ exercice/test.cs/test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bonjour Plamedie !");
+        Console.WriteLine("Ceci est un test simple en C#.");
+
+        // Exemple de fonction
+        int resultat = Addition(5, 7);
+        Console.WriteLine("Résultat de 5 + 7 = " + resultat);
+    }
+
+    static int Addition(int a, int b)
+    {
+        return a + b;
+    }
+}
